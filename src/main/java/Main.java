72e0d293729bbd85dd8877c4936b9d702dec7761
oleg.kundakov <oleg.kundakov.jazzteam@gmail.com>
2015-11-24 09:57:20 +0300

    3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.CouchbaseCluster;
+
 /**
  * Created by OlegKundakovJazzteam on 24.11.2015.
  */
 public class Main {
+    public static void main(String[] a) {
+        Cluster cluster = CouchbaseCluster.create();
+
+// Open the default bucket and the "beer-sample" one
+        Bucket defaultBucket = cluster.openBucket();
+//        Bucket beerSampleBucket = cluster.openBucket("beer-sample");
+    }
 }

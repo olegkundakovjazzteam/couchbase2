c795ba48fc4a02262ed32f9bebe4de17463c631a
oleg.kundakov <oleg.kundakov.jazzteam@gmail.com>
2015-11-24 13:54:14 +0300

    4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,47 @@
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.document.JsonDocument;
+import rx.Observable;
+import rx.functions.Action1;
+import rx.functions.Func1;
+
 
 /**
  * Created by OlegKundakovJazzteam on 24.11.2015.
  */
 public class Main {
-    public static void main(String[] a) {
-        Cluster cluster = CouchbaseCluster.create();
+    public static void main(String[] a) throws InterruptedException {
 
-// Open the default bucket and the "beer-sample" one
-        Bucket defaultBucket = cluster.openBucket();
-//        Bucket beerSampleBucket = cluster.openBucket("beer-sample");
+        Cluster cluster = CouchbaseCluster.create();
+        Bucket bucket = cluster.openBucket();
+
+//        JsonObject user = JsonObject.empty()
+//                .put("type", "car")
+//                .put("name", "jo")
+//                .put("age", 2)
+//                .put("color", "red");
+
+        bucket
+                .async()
+                .get("walter")
+                .flatMap(new Func1<JsonDocument, Observable<JsonDocument>>() {
+                    @Override
+                    public Observable<JsonDocument> call(final JsonDocument loaded) {
+                        loaded.content().put("age", 52);
+                        return bucket.async().replace(loaded);
+                    }
+                })
+                .subscribe(new Action1<JsonDocument>() {
+                    @Override
+                    public void call(final JsonDocument updated) {
+                        System.out.println("Updated: " + updated.id());
+                    }
+                });
+        Thread.sleep(1000);
+
+        cluster.disconnect();
+
+
     }
 }
